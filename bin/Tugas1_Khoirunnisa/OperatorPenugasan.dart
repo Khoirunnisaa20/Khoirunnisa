3cdc9f7b7c1492076d279bfc86715263333625e6
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-10 16:20:39 +0800

    Operator Penugasan

---
+++ bin/Tugas1_Khoirunnisa/OperatorPenugasan.dart
@@ -0,0 +1,32 @@
+
+ void main() {
+  int a = 10;
+  int b = 5;
+
+  a += b; 
+  print('a += b: $a'); 
+
+  a -= b; 
+  print('a -= b: $a'); 
+
+  a *= b; 
+  print('a *= b: $a'); 
+
+  a ~/= b; 
+  print('a ~/= b: $a'); 
+
+  a %= b; 
+  print('a %= b: $a'); 
+
+  a = 10; 
+  a &= b; 
+  print('a &= b: $a'); 
+
+  a = 10; 
+  a |= b; 
+  print('a |= b: $a'); 
+
+  a = 10; 
+  a ^= b; 
+  print('a ^= b: $a'); 
+}
