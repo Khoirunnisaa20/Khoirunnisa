72ef7b519988a51eb5e00669de7e5c799784b116
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-10 16:22:44 +0800

    Map

---
+++ bin/Tugas1_Khoirunnisa/Map.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  Map<String, String> person = {};
+  Map<String, String> product = {}; 
+  Map<String, String> address = {};
+
+  print("Person Map: $person");
+  print("Product Map: $product");
+  print("Address Map: $address");
+}
