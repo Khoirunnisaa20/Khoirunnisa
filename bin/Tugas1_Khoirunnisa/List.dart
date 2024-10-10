131d341b009c0cb384478cafe5d5095537ce292a
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-10 16:21:50 +0800

    List

---
+++ bin/Tugas1_Khoirunnisa/List.dart
@@ -0,0 +1,15 @@
+
+void main() {
+  List<int> intList = [];
+
+  List<String> stringList = [];
+
+  intList.add(1);
+  intList.add(2);
+  
+  stringList.add('Hello');
+  stringList.add('World');
+
+  print('List of int: $intList'); 
+  print('List of String: $stringList'); 
+}
