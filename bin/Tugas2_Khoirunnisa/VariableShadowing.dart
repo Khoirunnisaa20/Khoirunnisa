8a343b58dd3801f67dccda25469675f8e9c40a07
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-26 15:02:49 +0800

    VARIABLE SHADOWING

---
+++ bin/Tugas2_Khoirunnisa/VariableShadowing.dart
@@ -0,0 +1,14 @@
+
+void main() {
+  var orang = Orang("Nisa", "Banjarmasin");
+  print(
+      "Nama: ${orang.nama}, Alamat: ${orang.alamat}, Negara: ${orang.negara}");
+}
+
+class Orang {
+  String nama;
+  String? alamat;
+  final String negara = "Indonesia";
+
+  Orang(this.nama, this.alamat);
+}
