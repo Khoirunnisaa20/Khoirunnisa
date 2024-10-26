2d9375dc4f110a7b7f42ea0b47e0bb9e5d5fbaf0
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-26 15:35:26 +0800

    ERROR

---
+++ bin/Tugas2_Khoirunnisa/Error.dart
@@ -0,0 +1,15 @@
+
+void main() {
+  var list = ["Nisa", "Khoirunnisa", "Khoirunnisa"];
+
+  try {
+    if (list.length > 10) {
+      print(list[10]);
+    } else {
+      print(
+          "Indeks 10 tidak tersedia. Daftar hanya memiliki ${list.length} elemen.");
+    }
+  } catch (e) {
+    print("Terjadi kesalahan: $e");
+  }
+}
