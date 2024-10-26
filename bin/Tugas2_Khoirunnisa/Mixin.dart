548fb9dab6cbd0974d277e580283c0878a3f6eef
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-26 15:30:07 +0800

    MIXIN

---
+++ bin/Tugas2_Khoirunnisa/Mixin.dart
@@ -0,0 +1,29 @@
+
+mixin Playable {
+  String? name;
+
+  void play() {
+    print('Play $name');
+  }
+}
+
+mixin Stoppable {
+  String? name;
+
+  void stop() {
+    print('Stop $name');
+  }
+}
+
+class MediaPlayer with Playable, Stoppable {
+  MediaPlayer(String playerName) {
+    name = playerName;
+  }
+}
+
+void main() {
+  var player = MediaPlayer("Khoirunnisa");
+  
+  player.play(); 
+  player.stop(); 
+}
