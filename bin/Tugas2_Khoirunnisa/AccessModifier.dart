90a79082107916c12261798e534d91f563a03c7c
Khoirunnisaa20 <nisa.manis2003@gmail.com>
2024-10-26 15:26:52 +0800

    ACCES MODIFIER

---
+++ bin/Tugas2_Khoirunnisa/AccessModifier.dart
@@ -0,0 +1,20 @@
+
+class Product {
+  String? id;
+  String? name;
+  int? _quantity;
+
+  int? getQuantity() {
+    return _quantity; 
+  }
+}
+ //import 'data/produk.dart';
+void main() {
+  var product = Product();
+  product.id = "1";
+  product.name = "Acer";
+  product._quantity = 2; 
+  print("Product ID: ${product.id}");
+  print("Product Name: ${product.name}");
+  print("Product Quantity: ${product.getQuantity()}"); 
+}
